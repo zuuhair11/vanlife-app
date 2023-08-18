d540755d11475cefae42b5442f431552d9fb19a3
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2023-08-18 07:27:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom' ;
+import Home from './pages/Home' ;
+import About from './pages/About' ;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <header>
+                <Link className='site-logo' to='/'>#VanLife</Link>
+                
+                <nav>
+                    <Link to='/about'>About</Link>
+                </nav>
+            </header>
+            <Routes>
+                <Route 
+                    path='/'
+                    element={ <Home /> }
+                />
+                <Route 
+                    path='/about'
+                    element={ <About /> }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App;
+
+export default App ;
