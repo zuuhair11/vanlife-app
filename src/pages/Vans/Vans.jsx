27de880803b263cb3096d6e5a231c7f32b85057d
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2023-08-20 07:54:38 -0400

    added filtering vans feature using useSearchParams

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,9 +1,13 @@
-import { Link } from 'react-router-dom' ;
 import { useState, useEffect } from 'react' ;
+import { Link, useSearchParams } from 'react-router-dom' ;
 
 
 function Vans() {
     const [vans, setVans] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const typeFilter = searchParams.get('type');
+    console.log(typeFilter);
 
     useEffect( () => {
         fetch('/api/vans')
@@ -12,7 +16,11 @@ function Vans() {
 
     }, []);
 
-    const vanElements = vans.map( van => {
+    const displayedVans = typeFilter 
+        ? vans.filter( van => van.type.toLowerCase() === typeFilter.toLowerCase())
+        : vans
+
+    const vanElements = displayedVans.map( van => {
         return (
             <div key={ van.id } className='van-tile'>
                 <Link to={`/vans/${van.id}`} >
@@ -31,6 +39,36 @@ function Vans() {
     return (
         <div className='van-list-container'>
             <h1>Explore our van options</h1>
+
+            <div className='van-list-filter-buttons'>
+                <Link
+                    to='?type=simple'
+                    className='van-type simple'
+                >
+                    Simple
+                </Link>
+
+                <Link
+                    to='?type=luxury'
+                    className='van-type luxury'
+                >
+                    Luxury
+                </Link>
+
+                <Link
+                    to='?type=rugged'
+                    className='van-type rugged'
+                >
+                    Rugged
+                </Link>
+
+                <Link
+                    to='.'
+                    className='van-type clear-filters'
+                >
+                    Clear filter
+                </Link>
+            </div>
             
             <div className='van-list'>
                 { vanElements }
